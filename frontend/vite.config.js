0560aa17885834157f15d45b32550c2e9b86225d
Rafa Adnan <rafaadnan46@gmail.com>
2026-02-20 00:17:16 +0700

    vite config added base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/metadata-extract/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
